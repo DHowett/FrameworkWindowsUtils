51cc0f75dc151c5ca305cd9cdeb91ae86355870e
Dustin L. Howett <dustin@howett.net>
2022-01-06 22:58:41 -0600

    fauxect: readmem, query build version

--- FauxECTool/FauxECTool.cpp
+++ FauxECTool/FauxECTool.cpp
@@ -7,33 +7,82 @@
 #include <wil/resource.h>
 #include <wil/result_macros.h>
 #include "../CrosEC/Public.h"
-#include "../CrosEC/EC.h"
+
+struct cros_ec_command_v2 {
+	ULONG version;
+	ULONG command;
+	ULONG outsize;
+	ULONG insize;
+	ULONG result;
+#pragma warning(suppress: 4200)
+	UCHAR data[0];
+};
+
+struct cros_ec_readmem_v2 {
+	ULONG offset;
+	ULONG bytes;
+	UCHAR buffer[0xFF];
+};
+
+void hexdump(const uint8_t* data, int len)
+{
+	int i, j;
+
+	if (!data || !len)
+		return;
+
+	for (i = 0; i < len; i += 16) {
+		/* Left column (Hex) */
+		for (j = i; j < i + 16; j++) {
+			if (j < len)
+				fprintf(stderr, " %02x", data[j]);
+			else
+				fprintf(stderr, "   ");
+		}
+		/* Right column (ASCII) */
+		fprintf(stderr, " |");
+		for (j = i; j < i + 16; j++) {
+			int c = j < len ? data[j] : ' ';
+			fprintf(stderr, "%c", isprint(c) ? c : '.');
+		}
+		fprintf(stderr, "|\n");
+	}
+}
 
 int main()
 {
-    try {
-        wil::unique_hfile device;
-        device.reset(CreateFileW(LR"(\\.\GLOBALROOT\Device\CrosEC)", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, 0, nullptr));
-        THROW_LAST_ERROR_IF(!device);
+	try {
+		wil::unique_hfile device;
+		device.reset(CreateFileW(LR"(\\.\GLOBALROOT\Device\CrosEC)", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, 0, nullptr));
+		THROW_LAST_ERROR_IF(!device);
 
-        cros_ec_command_v2 *cmd = (cros_ec_command_v2*)malloc(sizeof(cros_ec_command_v2) + 255);
-        cmd->command = 0x11;
-        cmd->insize = 0;
-        cmd->outsize = 255;
-        cmd->result = 0xff;
-        cmd->version = 0;
+		
+		cros_ec_command_v2* cmd = (cros_ec_command_v2*)calloc(1, sizeof(cros_ec_command_v2) + 255);
+		cmd->command = 0x4;
+		cmd->insize = 255;
+		cmd->outsize = 0;
+		cmd->result = 0xff;
+		cmd->version = 0;
 
-        DWORD retb{};
-        THROW_LAST_ERROR_IF(!DeviceIoControl(device.get(), IOCTL_CROSEC_XCMD, cmd, sizeof(cros_ec_command_v2) + 255, cmd, sizeof(cros_ec_command_v2) + 255, &retb, nullptr));
-        std::cout << "Hello World!\n";
-        std::cout << cmd->command << "|" << cmd->result << std::endl;
-    }
-    catch (std::runtime_error e) {
-        std::cerr << e.what() << std::endl;
-    }
-    catch (...) {
-        LOG_CAUGHT_EXCEPTION();
-    }
+		std::cout << "OUT: " << cmd->command << "|" << cmd->result << std::endl;
+		DWORD retb{};
+		THROW_LAST_ERROR_IF(!DeviceIoControl(device.get(), IOCTL_CROSEC_XCMD, cmd, sizeof(cros_ec_command_v2) + 255, cmd, sizeof(cros_ec_command_v2) + 255, &retb, nullptr));
+		std::cout << "IN : " << cmd->command << "|" << cmd->result << std::endl;
+		std::cout << "IN_: " << (const char*)cmd->data << std::endl;
+		
+		cros_ec_readmem_v2 rm{};
+		rm.bytes = 0xfe;
+		rm.offset = 0;
+		//DWORD retb{};
+		THROW_LAST_ERROR_IF(!DeviceIoControl(device.get(), IOCTL_CROSEC_RDMEM, &rm, sizeof(cros_ec_readmem_v2), &rm, sizeof(cros_ec_readmem_v2), &retb, nullptr));
+		hexdump(rm.buffer, rm.bytes);
+	}
+	catch (std::runtime_error e) {
+		std::cerr << e.what() << std::endl;
+	}
+	catch (...) {
+		LOG_CAUGHT_EXCEPTION();
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
